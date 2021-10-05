da5a992a24d9ef210d200bf87bb2f152f6fcf8f9
bhawnachaudhary <bhawnachaudhary626@gmail.com>
2021-10-05 15:18:33 +0530

    updated

--- Web Mining/Scraping_new_headlines.py
+++ Web Mining/Scraping_new_headlines.py
@@ -1,7 +1,5 @@
-import sys
 from selenium import webdriver
 from webdriver_manager import driver
-import webdriver_manager
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
 
@@ -11,7 +9,7 @@ webpage = "https://www.ndtv.com/latest#pfrom=home-ndtv_mainnavgation"
 driver.get(webpage)
 
 target = driver.find_element_by_css_selector('div.lisingNews')  # single
-news = target.find_element_by_css_selector('div.newsItm')   # multi
+news = target.find_element_by_css_selector('div.news_Itm')   # multi
 
 # loop through all the news
 data = []
@@ -34,16 +32,10 @@ for item in news:
         'posted_by':posted_by,
         'content':content
     })
-
+    #loop ends
+    
 if len(data) > 0:
     pd.DataFrame(data).to_csv('todays_headlines.csv')
 else:
     print("No news headlines found!!")
 driver.close()
-
-#     print(title)
-#     print(posted_by)
-#     print()
-
-# print(len(news),'news found')
-# driver.close()
